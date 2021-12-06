d40dd6173c9f8e55b52e7b637de4f8397ec4251b
Mideolabanji <olabanjikaosara@yahoo.com>
2021-12-06 16:37:47 +0100

    made the navbar links black on click

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-$(".carousel").carousel();
+var myCarousel = document.querySelector("#myCarousel");
+var carousel = new bootstrap.Carousel(myCarousel);
+
+function underlineAbout() {
+  underlineHomeLink.classList.remove("active");
+  underlineContactLink.classList.remove("active");
+  underlineAboutLink.classList.add("active");
+}
+
+function underlineContact() {
+  underlineHomeLink.classList.remove("active");
+  underlineAboutLink.classList.remove("active");
+  underlineContactLink.classList.add("active");
+}
+
+function underlineHome() {
+  underlineAboutLink.classList.remove("active");
+  underlineContactLink.classList.remove("active");
+  underlineHomeLink.classList.add("active");
+}
+
+let underlineHomeLink = document.querySelector("#homeLink");
+let underlineAboutLink = document.querySelector("#aboutLink");
+let underlineContactLink = document.querySelector("#contactLink");
+
+underlineHomeLink.addEventListener("click", underlineHome);
+underlineAboutLink.addEventListener("click", underlineAbout);
+underlineContactLink.addEventListener("click", underlineContact);
